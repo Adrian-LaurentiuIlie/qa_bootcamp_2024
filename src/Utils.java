e2a47e2e88e96a39294c182828bda2ec98c357c2
ilie.adrian1@outlook.com <ilie.adrian1@outlook.com>
2024-10-15 10:36:53 +0300

    Added Homework

--- src/Utils.java
+++ src/Utils.java
@@ -3,4 +3,9 @@ public class Utils {
     public static void printSquare(Square sq){
         System.out.println("Square with side " + sq.getSquareSide() +" has area of " + sq.getArea());
     }
+
+    public static void bookDetails(Book bk){
+        System.out.println("Book " + bk.getName() + " (" + bk.getPrice() + " RON), by " + bk.getAuthor().getName()
+                + ", published in " + bk.getYear() + ".");
+    }
 }
